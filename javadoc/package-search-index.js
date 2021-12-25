87a1941d4607593b592fa2bab66ad87d045ee9d4
BuildTools <unconfigured@null.spigotmc.org>
2021-12-25 17:42:33 -0600

    Add Javadoc and fix release number

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.infinitepower563.simplenet.example"},{"l":"io.github.infinitepower563.simplenet.exception"},{"l":"io.github.infinitepower563.simplenet.net"},{"l":"io.github.infinitepower563.simplenet.util"}];updateSearchResults();
